0af8f7db494cccee1456f6d2ac58a7222c907924
Eren Küçük <erenkucuk2002@gmail.com>
2023-02-03 14:38:52 +0300

    ek add

---
+++ ek/StdManangementSystem/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StdManangementSystem
+{
+    public abstract class Person
+    {
+        protected string name;
+        protected string surname;
+    }
+}
---
+++ ek/StdManangementSystem/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StdManangementSystem
+{
+    public class Student : Person
+    {
+        prop
+    }
+}
+
+/*
+ private string StudentID;
+        private string Name;
+        private string Major;
+        private string Gender;
+        private DateTime DOB;
+        private string Email;
+        private string Address;
+        private string ContactNo;
+
+        public void CreateStudent(String StudentID)
+        {
+
+        }
+        public void GetStudentInfo()
+        {
+           
+        } 
+*/
